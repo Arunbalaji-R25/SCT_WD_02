843f6a1d7727333f1e7f43cc2f1f587999a9bc21
Arunbalaji-R25 <arunbalaji.r.25@gmail.com>
2025-02-19 09:27:46 +0530

    Add files via upload

---
+++ script c.js
@@ -0,0 +1,22 @@
+var display = document.getElementById("display");
+
+function appendToDisplay(input)
+{
+    display.value += input;
+} 
+
+function cleardisplay()
+{
+    display.value="";
+}
+ 
+function calculate()
+{
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "syntax error";
+    }
+    
+}
